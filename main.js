f94b08ba516102e9cec50d863d5790704e8fa975
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-07-02 14:23:50 -0500

    Archivo tipo modular
    Se crea este archivo con la intecion de que las conexiones se vean limpias y solo se llamen funciones

---
+++ main.js
@@ -0,0 +1,9 @@
+import { ctx, myChart } from "./scripts/chart.js";
+import { getApiCGGlobal,getApiOne, getApiTree, getApiTwo } from "./scripts/api.js";
+
+ctx;
+myChart;
+getApiCGGlobal();
+getApiOne();
+getApiTwo();
+getApiTree();
