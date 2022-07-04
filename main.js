658f190deffd3b3f64924a838b6c598ce2941827
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-07-03 19:21:17 -0500

    actualizacion de chart

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-import { ctx, myChart } from "./scripts/chart.js";
+import { ctx, myChart ,ctxOne,myChartOne } from "./scripts/chart.js";
 import { getApiCGGlobal,getApiOne, getApiTree, getApiTwo } from "./scripts/api.js";
 
 ctx;
 myChart;
+ctxOne;
+myChartOne;
 getApiCGGlobal();
 getApiOne();
 getApiTwo();
--- scripts/chart.js
+++ scripts/chart.js
@@ -56,5 +56,35 @@ export const myChart = async () => {
     },
   });
 };
+export const ctxOne = document.getElementById("myChartApp").getContext("2d");
+export const myChartOne = new Chart(ctxOne, {
+  type: "line",
+  data: {
+    labels: [
+      "cripto1",
+      "cripto2",
+      "cripto3",
+      "cripto4",
+      "scripto5",
+      "scripto6",
+    ],
+    datasets: [
+      {
+        label: "Ranking en tiempo real",
+        data: [20, 15, 45, 26, 76, 55],
+        backgroundColor: ["#F4D22C", "#D9BA82"],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
 
 myChart();
+
