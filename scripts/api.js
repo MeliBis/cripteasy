07b31748929d03228be3c080afd7d5e54bd0d7dd
Daniel Ninacondor Narvaez <danielnn.aqp@gmail.com>
2022-07-04 23:57:39 -0500

    Conexion a top criptomonedas

--- scripts/api.js
+++ scripts/api.js
@@ -56,4 +56,32 @@ export const getApiTree =async ()=>{
     console.log("hay un error en api Tree");
   }
 };
+// Seleccion del top 10 de criptomonedas
+const seleccionarCriptomonedas = document.querySelector('#criptomonedas');
+  // Creando un Promise
+const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
+  resolve(criptomonedas);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  consultarCriptomonedas();
+})
+function consultarCriptomonedas(){
+   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+   fetch(url)
+    .then( respuesta => respuesta.json() )
+    .then( resultado  => obtenerCriptomonedas(resultado.Data))
+    .then( criptomonedas => selectCriptomonedas(criptomonedas) )
+    //.catch( error => console.log(error));
+}
+
+function selectCriptomonedas(criptomonedas){
+  criptomonedas.forEach( cripto => {
+    const { FullName, Name} = cripto.CoinInfo;
+    const option = document.createElement('option');
+    option.value = Name;
+    option.textContent = FullName;
+    seleccionarCriptomonedas.appendChild(option);
+  });
+}
 
