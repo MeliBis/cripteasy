a02a9b911f6041b2adca6b0591d4696950a34516
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-07-06 23:51:35 -0500

    Se corrigen cambios de la grafica

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import { ctx, myChart ,ctxOne,myChartOne } from "./scripts/chart.js";
+import { ctx, myChart /*,ctxOne,myChartOne*/ } from "./scripts/chart.js";
 import { getApiCGGlobal,getApiOne, getApiTree, getApiTwo } from "./scripts/api.js";
 
 ctx;
 myChart;
-ctxOne;
-myChartOne;
+// ctxOne;
+// myChartOne;
 getApiCGGlobal();
 getApiOne();
 getApiTwo();
