3c8054f7d7f9c8db3acd1e6e5b4f8c89fe7c13de
Melissa Barrios <melissa.blecona@gmail.com>
2022-07-11 22:04:43 -0500

    texto animado

---
+++ scripts/typing.js
@@ -0,0 +1,18 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="cripto">Cripteasy</i>',
+		
+	],
+
+	typeSpeed: 75, 
+	startDelay: 100, 
+	backSpeed: 150, 
+	smartBackspace: true, 
+	shuffle: false, 
+	backDelay: 1000, 
+	loop: true, 
+	loopCount: false, 
+	showCursor: true, 
+	cursorChar: '|', 
+	contentType: 'html', 
+});
